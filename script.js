10ade4e5232fdebee80c1634a7a519439810ef0c
estefa <nicole2001zam@gmail.com>
2023-09-28 02:46:51 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let signUp = document.getElementById("signUp");
+let signIn = document.getElementById("signIn");
+let nameInput = document.getElementById("nameInput");
+let title = document.getElementById("title");
+
+signIn.onclick = function() {
+    nameInput.style.maxHeight = "0";
+    title.innerHTML = "Login";
+    signUp.classList.add("disable");
+    signIn.classLidt.renove("disable");
+}
+
+signUp.onclick = function() {
+    nameInput.style.maxHeight = "60px";
+    title.innerHTML = "Registro";
+    signUp.classList.renove("disable");
+    signIn.classLidt.add("disable");
+}
